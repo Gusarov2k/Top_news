a2689116c6f407c5208e58e611134712ac6480dc
gusarov2k <motoface@yandex.ru>
2019-03-29 06:36:54 +0300

    plus 30 minutes

--- spec/factories/top_up_news.rb
+++ spec/factories/top_up_news.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :top_up_news do
-    title { "MyString" }
-    announcement { "MyString" }
-    context { "MyText" }
+    title { "MyString title" }
+    announcement { "MyString is announcement" }
+    context { "MyText is bla bla bla bla bla bla" }
     created { "2019-03-28 21:39:39" }
   end
 end
